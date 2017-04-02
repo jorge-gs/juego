33bf48656c83f155d6de84d3591afe8cb7b20a12
jorge-gs <jorgeluis.garcia@ujcv.edu.hn>
2017-04-02 11:32:15 -0600

    Agregar limites
    Agrega los limites a la escena y los posiciona correctamente

---
+++ Assets/Scripts/ColocarLimites.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColocarLimites : MonoBehaviour {
+	public GameObject inferiror;
+	public GameObject superior;
+	public GameObject izquierdo;
+	public GameObject derecho;
+	public Camera camara;
+
+	// Use this for initialization
+	void Start () {
+		//Obtener dimensiones en pixeles
+		int anchoPixeles = Screen.width;
+		int altoPixeles = Screen.height;
+
+		//Obtener dimensiones en puntos
+		float razonPixelesPuntos = altoPixeles / (2 * this.camara.orthographicSize);
+		float anchoPuntos = anchoPixeles / razonPixelesPuntos;
+		float altoPuntos = anchoPixeles / razonPixelesPuntos;
+
+		this.derecho.transform.position = new Vector3 ((anchoPuntos + 1) / 2, 0);
+		this.izquierdo.transform.position = new Vector3 (-(anchoPuntos + 1) / 2, 0);
+
+		this.superior.transform.localScale = new Vector3 (anchoPuntos, 1, 1);
+		this.inferiror.transform.localScale = new Vector3 (anchoPuntos, 1, 1);
+
+		Debug.Log (this.izquierdo.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour {
+	private float tiempo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.tiempo > 5) {
+			this.tiempo = (this.tiempo - 5) + Time.deltaTime;
+		} else {
+			this.tiempo += Time.deltaTime;
+		}
+
+		Vector3 posicion = 1.25f * new Vector3 (Mathf.Cos(2 * Mathf.PI / 5 * tiempo), Mathf.Sin(2 * Mathf.PI / 5 * tiempo));
+		float angulo = Mathf.Atan2 (posicion.y, posicion.x);
+
+		this.gameObject.GetComponent<Rigidbody2D> ().position = posicion;
+		this.gameObject.transform.rotation = Quaternion.Euler(0, 0, angulo);
+
+		Debug.Log (this.gameObject.transform.rotation);
+	}
+}
