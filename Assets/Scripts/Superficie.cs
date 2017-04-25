298af6146b301fd47c46dc3eec8ea795d68f2e70
jorge-gs <jorgeluis.garcia@ujcv.edu.hn>
2017-04-25 16:01:06 -0600

    Agregar animaciones botones

--- Assets/Scripts/Superficie.cs
+++ Assets/Scripts/Superficie.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Superficie : MonoBehaviour {
+public class Superficie : MonoBehaviour, IPointerClickHandler {
 	public static event Click OnClick;
 	public delegate void Click();
 
-	void OnMouseDown() {
+	void IPointerClickHandler.OnPointerClick (PointerEventData eventData)
+	{
 		if (OnClick != null) {
 			OnClick ();
 		}
