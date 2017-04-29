cb5c9a270c18c31073c9d1c06d768c125eee2054
alexanderchele <alexander.caballero@ujcv.edu.hn>
2017-04-28 19:18:19 -0600

    Agregar panel de informacion y panel de animaciones

---
+++ Assets/Scripts/UI/Atras.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atras : MonoBehaviour {
+	public GameObject padre;
+	public GameObject panel;
+
+	public void OnClick() {
+		this.padre.GetComponent<Animator> ().SetBool ("Mostrar", false);
+		Invoke ("OnAnimacionTermino", 0.5f);
+	}
+
+	public void OnAnimacionTermino() {
+		this.padre.SetActive (false);
+		this.panel.SetActive (true);
+	}
+}
