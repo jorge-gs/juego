bcd959993ba42d25aeaf06d24c1d35b6e46e39a0
alexanderchele <alexander.caballero@ujcv.edu.hn>
2017-04-28 18:11:57 -0600

    Agregar menú para perder

---
+++ Assets/Scripts/UI/Continuar.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Continuar : MonoBehaviour {
+	public void OnClick() {
+
+	}
+}
---
+++ Assets/Scripts/UI/Puntuacion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puntuacion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var texto = this.gameObject.GetComponent<Text> ();
+		texto.text = "" + PlayerPrefs.GetInt ("Puntuacion");
+	}
+}
